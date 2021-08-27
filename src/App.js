b36dc7507d694f1126c6ec4595c44476dccf0c43
zaidm124 <zaidm124@gmail.com>
2021-08-27 19:52:01 +0530

    Firebase

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Chat from "./myComponents/Chat";
+import First from "./myComponents/first_page";
 import Login from "./myComponents/Login";
 import SideBar from "./myComponents/SideBar";
 
 function App() {
   const [user, setUser] = useState(true);
   return (
-    <div className="app">
-      <div className="app_body">
-        <Router>
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <SideBar />
-              <Chat />
-            </Route>
-            <Route path="/rooms">
-              <SideBar />
-            </Route>
-            <Route path="/">
-              <Login />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+    <div className="App">
+      <First />
+      {/* <div className="app_body"> */}
+      {/* <Router>
+        <Switch>
+          <Route path="/rooms/:roomId">
+            <SideBar />
+            <Chat />
+          </Route>
+          <Route path="/rooms">
+            <SideBar />
+          </Route>
+          <Route path="/">
+            <First />
+          </Route>
+        </Switch>
+      </Router> */}
+      {/* </div> */}
     </div>
   );
 }
---
+++ src/myComponents/first_page.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./firstPage.css";
+function First() {
+  return (
+    <div class="first_body">
+      <div className="welcome_first">
+        <div className="welcome_first_nav">
+          <div>
+            <i class="fas fa-search"></i>
+          </div>
+          <div>
+            <i class="far fa-registered"></i>
+          </div>
+          <div>
+            <i class="fas fa-comments"></i>
+          </div>
+        </div>
+        <h2>Hello visitor Welcome To the IIIT SUART ALUMNI website</h2>
+        <div className="instruction_first">
+          <h3>
+            Instructions ! <i class="fas fa-graduation-cap"></i>
+          </h3>
+          <ol>
+            <li>
+              If you want to register as an alumni then go down to the alumni
+              section and register your self.
+            </li>
+            <li>
+              If you want to search for any alumni details then go down to
+              search alumni section and enter detail.
+            </li>
+          </ol>
+        </div>
+      </div>
+      <div className="second_first">
+        <div className="search_alumni">
+          <div className="search_alumni_head">
+            <h2>Sreach Alumni</h2>
+          </div>
+          <div className="input_search_alumni">
+            <div>
+              <label>Student Id</label>
+              <input type="text"></input>
+            </div>
+            <div>
+              <label>Batch</label>
+              <input type="number"></input>
+            </div>
+            <div>
+              <label>Name</label>
+              <input type="text"></input>
+            </div>
+            <div className="alumni_search">
+              <button>search</button>
+            </div>
+          </div>
+        </div>
+        <div className="alumni_section">
+          <div>
+            <h2>Register / Login Here</h2>
+          </div>
+          <div className="alumni_search cc">
+            <button>Login</button>
+            <button>Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default First;
