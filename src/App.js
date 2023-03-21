f999ff24ac2f108e53a41ef8ecbfbdb1861bc331
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2023-03-22 00:34:20 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,51 +10,12 @@ import StudentInfo from "./myComponents/StudentInfo";
 
 import Profile from "./myComponents/Profile_for_viewer";
 import Profilealumni from "./myComponents/Profile_for_alumni";
+import AfterSearch from "./myComponents/AfterSearch";
 
 function App() {
   const [user, setUser] = useState(true);
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <First />
-          </Route>
-          <Route exact path="/profile">
-              <Profile/>
-          </Route>
-          <Route exact path="/profilealumni">
-              <Profilealumni />
-          </Route>
-          <Route exact path="/register">
-            {" "}
-          </Route>
-          <Route exact path="/login">
-            {" "}
-          </Route>
-          <Route exact path="/search">
-            <Search />
-          </Route>
-          <Route path="/student"><StudentInfo/></Route>
-        </Switch>
-      </Router>
-      {/* <div className="app_body"> */}
-      {/* <Router>
-        <Switch>
-          <Route path="/rooms/:roomId">
-            <SideBar />
-            <Chat />
-          </Route>
-          <Route path="/rooms">
-            <SideBar />
-          </Route>
-          <Route path="/">
-            <First />
-          </Route>
-        </Switch>
-      </Router> */}
-      {/* </div> */}
-    </div>
+    <AfterSearch/>
   );
 }
 
---
+++ src/myComponents/AfterSearch.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./AfterSearch.css"
+
+function AfterSearch() {
+    return (
+        <div>
+            <div>
+            <div class="Background"> 
+            <div className="Title">
+                <h2>Search Results</h2>
+                
+            </div>
+            <div className="ProfileBackground">
+
+              <div className="Profile">
+                <div>
+                  <h3>Name</h3>
+                </div>
+                <div>
+                  <h3>Batch</h3>
+                </div>
+                <div>
+                  <h3>Addmission No</h3>
+                </div>
+                
+              </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    )
+}
+
+export default AfterSearch
